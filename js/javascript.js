9a06cde38b3624649c7e401c753045a82e1f8574
DesleyAalderink <desley-aalderink@hotmail.com>
2018-05-23 10:35:33 +0200

    herkansing cssttr

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+var change = document.querySelectorAll('p')[6]
+var newtext = document.querySelectorAll('p')[7]
+
+
+function toggletext(){
+  change.classList.toggle('none')
+  newtext.classList.toggle('none')
+}
+
+function changenewtext(){
+  newtext.classList.toggle('none')
+}
+
+function bgchange(){
+  document.querySelector('body').classList.toggle('bg')
+}
+
+setInterval(function() {
+  toggletext()
+}, 3000);
+
+document.querySelectorAll('button')[1].addEventListener("click", function(e){
+  e.preventDefault()
+})
+
+document.querySelectorAll('button')[2].addEventListener("click", function(e){
+  e.preventDefault()
+})
+
+document.querySelectorAll('button')[2].addEventListener("click", function(){
+  bgchange();
+})
